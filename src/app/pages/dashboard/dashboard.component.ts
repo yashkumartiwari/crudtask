823ff0ec461009d5a0157bdc434895584743396f
yashkumartiwari <kumaryash6249@gmail.com>
2022-03-23 10:10:34 +0530

    fixture task added on the dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -16,20 +16,26 @@ import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation
 })
 export class DashboardComponent implements OnInit {
 
-  columnsToDisplay: string[] = [
-    'firstName',
-    'email',
-    'action',
-  ];
+  // columnsToDisplay: string[] = [
+  //   'firstName',
+  //   'email',
+  //   'action',
+  // ];
 
-  dataSource: MatTableDataSource<User>;
-  totalCount: number;
+  // dataSource: MatTableDataSource<User>;
+  // totalCount: number;
 
 
-  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  // @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  // @ViewChild(MatSort) sort: MatSort;
  
-  allUserList: User[];
+  // allUserList: User[];
+  // numbers = [1,2,3,4,5,6,7,8,9,0]
+  // operators = ["+","-","x","/"]
+  // value: string;
+  numberOfPlayers:number;
+  numberOfRounds: number[] = [];
+  show: boolean;
 
   constructor(
     private router: Router,
@@ -38,48 +44,62 @@ export class DashboardComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.getTableData();
+    // this.getTableData();
   }
-  openDialog(element: any) {
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent);
-    // const snack = this.snackBar.open('Snack bar open before dialog');
+//   openDialog(element: any) {
+//     const dialogRef = this.dialog.open(ConfirmationDialogComponent);
+//     // const snack = this.snackBar.open('Snack bar open before dialog');
 
-    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
-      if (confirmed) {
-        this.onDelete(element);
-      }
-    });
+//     dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+//       if (confirmed) {
+//         this.onDelete(element);
+//       }
+//     });
+//   }
+
+// async getTableData() {
+
+//     this.allUserList = await this._httpService.getAllUsers();
+//     this.dataSource = new MatTableDataSource<User>(
+//       this.allUserList
+//     );
+//     this.totalCount = this.allUserList.length;
+//     this.dataSource.paginator = this.paginator;
+//     this.dataSource.sort = this.sort;
+    
+//   }
+
+  
+//   onAdd(){
+//     this.router.navigate(['add-edit','add','Add User']);
+//   } 
+//   onEdit(element: User) {
+//     this.router.navigate(['add-edit','edit',element.email]);
+//   }  
+//   onDelete(element:User) {
+//     this.allUserList.forEach( (user, index) => {
+//       if(user === element) this.allUserList.splice(index,1);
+//     });
+//     if(this._httpService.deleteUser(this.allUserList)){
+     
+//       alert("User Deleted Successfully")
+//       this.getTableData();
+//     }
+//   } 
+  gameNumber(i){
+    return new Array(i)
   }
-
-async getTableData() {
-
-    this.allUserList = await this._httpService.getAllUsers();
-    this.dataSource = new MatTableDataSource<User>(
-      this.allUserList
-    );
-    this.totalCount = this.allUserList.length;
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  showFixture(){
+   this.numberOfRounds = []
+   let tempnumberOfPlayer =this.numberOfPlayers;
+    for (var i = 0; tempnumberOfPlayer >= 2; i++){
+      tempnumberOfPlayer = Math.round(tempnumberOfPlayer/2)
+      this.numberOfRounds.push(tempnumberOfPlayer);
+    }
+    this.show = true;
     
   }
 
-  
-  onAdd(){
-    this.router.navigate(['add-edit','add','Add User']);
-  } 
-  onEdit(element: User) {
-    this.router.navigate(['add-edit','edit',element.email]);
-  }  
-  onDelete(element:User) {
-    this.allUserList.forEach( (user, index) => {
-      if(user === element) this.allUserList.splice(index,1);
-    });
-    if(this._httpService.deleteUser(this.allUserList)){
-     
-      alert("User Deleted Successfully")
-      this.getTableData();
-    }
-  } 
 }
 
 
